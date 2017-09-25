e189e6058efbc71f33a439c03c62f67d9781a396
zocha_sz <zofia.szczudlik@gmail.com>
2017-09-25 11:05:27 +0200

    feat: update user.model with passport

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -70,6 +71,31 @@ const userSchema = new mongoose.Schema({
     }]
 }, {timestamps: true});
 
+userSchema.pre('save', function save(next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) { return next(err); }
+        else {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) { return next(err); }
+                else {
+                    user.password = hash;
+                    return next();
+                }
+            })
+        }
+    })
+});
+
+userSchema.methods.checkPassword = function (password, cb) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        cb(err, isMatch);
+    });
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
