920684656c510b15d64432a3c3fdddaf258984c0
zocha_sz <zofia.szczudlik@gmail.com>
2017-09-25 10:52:50 +0200

    feat: update app.js with db.configs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 // const cors = require('cors');
 
-// require('./configs/db.config');
+require('./configs/db.config');
 
 // const corsOptions = require('./configs/cors.config');
 
