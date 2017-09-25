5bcfcfd04f0a7811f57ad51129fc7c64489e2330
zocha_sz <zofia.szczudlik@gmail.com>
2017-09-25 10:46:05 +0200

    feat: add db.config

---
+++ configs/db.config.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const dbName = 'cube-api';
+const dbUri = `mongodb://localhost/${dbName}`;
+
+mongoose.connect(dbUri, { useMongoClient: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log(`Connected to the ${dbName} database`);
+});
